5ab53347cecb26f230ea1cd50d59c5d7034b6c0f
Tingo <findjohntg@gmail.com>
2019-11-29 18:56:50 +0800

    Create kuaizhi.js

---
+++ kuaizhi.js
@@ -0,0 +1,24 @@
+//https:\/\/kz\.sync163\.com\/api_v2\/user\/(pro-jobs-count|info)
+
+const path1 = "pro-jobs-count";
+const path2 = "info";
+
+const url = $request.url;
+let obj = JSON.parse($response.body);
+
+if(url.indexOf(path1) != -1) {
+	obj.total_pro_jobs_count = 99;
+	obj.availabe_pro_jobs_count = 99;
+	obj.total_invite_count = 99;
+	obj.available_invite_count =99;
+}
+
+if(url.indexOf(path2) != -1) {
+	obj.userinfo.experiencer = 0;
+	obj.userinfo.vip = 1;
+	obj.userinfo.vip_expired_at = "2099-1-1";
+	obj.userinfo.vip_expired = false;
+	obj.userinfo.vip_if_gift = false;
+}
+
+$done({body: JSON.stringify(obj)});
