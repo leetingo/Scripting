cdf13131da976d11a919dc6735d81372a48470fd
Tingo <findjohntg@gmail.com>
2019-11-29 19:03:53 +0800

    Update kuaizhi.js

--- kuaizhi.js
+++ kuaizhi.js
@@ -8,7 +8,7 @@ let obj = JSON.parse($response.body);
 
 if(url.indexOf(path1) != -1) {
 	obj.total_pro_jobs_count = 99;
-	obj.availabe_pro_jobs_count = 99;
+	obj.available_pro_jobs_count = 99;
 	obj.total_invite_count = 99;
 	obj.available_invite_count =99;
 }
@@ -16,7 +16,7 @@ if(url.indexOf(path1) != -1) {
 if(url.indexOf(path2) != -1) {
 	obj.userinfo.experiencer = 0;
 	obj.userinfo.vip = 1;
-	obj.userinfo.vip_expired_at = "2099-1-1";
+	obj.userinfo.vip_expired_at = 4070880000;
 	obj.userinfo.vip_expired = false;
 	obj.userinfo.vip_if_gift = false;
 }
