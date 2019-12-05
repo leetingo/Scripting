d09f86f1e3e752c3b15e297a11781ccfe42151c1
Tingo <findjohntg@gmail.com>
2019-12-05 11:35:15 +0800

    Update cpumonitor.js

--- cpumonitor.js
+++ cpumonitor.js
@@ -3,14 +3,15 @@ obj = {
 	"errno": 0,
 	"errmsg": "OK",
 	"data": {
-	"userId": "11835468",
-	"nick": "小火箭i0cv1m",
-	"score": "114514",
-	"expireTime": "4070880000",
-	"lastSignTime": "1575515952",
-	"lastBuyTime": "1575516435",
-	"lastRewardTime": "0",
-	"signDays": "114514",
-	"isVip": true
- }
-}
+		"userId": "11835468",
+		"nick": "小火箭i0cv1m",
+		"score": "114514",
+		"expireTime": "4070880000",
+		"lastSignTime": "1575515952",
+		"lastBuyTime": "1575516435",
+		"lastRewardTime": "0",
+		"signDays": "114514",
+		"isVip": true
+	}
+};
+$done({body: JSON.stringify(obj)});
