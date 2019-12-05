03bce4cf243b97bc9129b327ba1d9a0ea213b3fe
Tingo <findjohntg@gmail.com>
2019-12-05 11:32:24 +0800

    Create cpumonitor.js

---
+++ cpumonitor.js
@@ -0,0 +1,16 @@
+let obj = JSON.parse($response.body);
+obj = {
+	"errno": 0,
+	"errmsg": "OK",
+	"data": {
+	"userId": "11835468",
+	"nick": "小火箭i0cv1m",
+	"score": "114514",
+	"expireTime": "4070880000",
+	"lastSignTime": "1575515952",
+	"lastBuyTime": "1575516435",
+	"lastRewardTime": "0",
+	"signDays": "114514",
+	"isVip": true
+ }
+}
