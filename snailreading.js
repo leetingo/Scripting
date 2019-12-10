95df47c3d410c8cf94e5dab7b97e67e6b15a8b0e
Tingo <findjohntg@gmail.com>
2019-12-10 17:32:03 +0800

    Update snailreading.js

--- snailreading.js
+++ snailreading.js
@@ -13,8 +13,6 @@ Surge & QX MITM = p.du.163.com
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.tradeEndTime = 1679685290000;
+obj.tradeEndTime = 4070880000000;
 body = JSON.stringify(obj);
 $done({body});
-
-//By yxiaocai & JO2EY
