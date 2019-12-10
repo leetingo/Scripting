d2ac7811ab812f0885d944cbdd1dd27cad474cfd
Tingo <findjohntg@gmail.com>
2019-12-10 19:45:25 +0800

    update doubanreading

---
+++ douban_book.js
@@ -0,0 +1,5 @@
+let obj = JSON.parse($response.body);
+obj.vip.can_read = true;
+obj.has_owned = true;
+$done({body: JSON.stringify(obj)});
+//https:\/\/read\.douban\.com\/api\/v2\/works\/\d+($|\?x_includes=has_owned%2Cprice%2Cis_in_library%2Cvip%2Cpromotion)
