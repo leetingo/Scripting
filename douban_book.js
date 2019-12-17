c98a882cb9f14094ff07c08445681c1ab9c6b7a2
Tingo <findjohntg@gmail.com>
2019-12-17 16:59:36 +0800

    Update douban_book.js

--- douban_book.js
+++ douban_book.js
@@ -1,4 +1,9 @@
 let obj = JSON.parse($response.body);
 obj.has_owned = true;
+obj.vip = {
+    "price": 319,
+    "discount": 80,
+    "can_read": true
+  };
 $done({body: JSON.stringify(obj)});
 //https:\/\/read\.douban\.com\/api\/v2\/works\/\d+($|\?x_includes=has_owned%2Cprice%2Cis_in_library%2Cvip%2Cpromotion)
