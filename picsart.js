2a778b76305c752f23e15fa4c6f9c4c5c5097b45
Tingo <TingoLee@users.noreply.github.com>
2019-12-18 10:05:27 +0800

    Update picsart.js

--- picsart.js
+++ picsart.js
@@ -1,4 +1,4 @@
-//https:\/\/api\.meiease\.cn\/(users\/show\/me\.json|shop\/subscription\/validate)
+//^https:\/\/api\.meiease\.cn\/(users\/show\/me\.json$|shop\/subscription\/validate)
 
 let obj = JSON.parse($response.body)
 let url = $request.url;
@@ -8,11 +8,13 @@ const path2 = "shop/subscription/validate"
 
 if(url.indexOf(path1) != -1) {
 	obj.verified_type = "subscribed"
+	obj.subscription.granted = true
 }
 if(url.indexOf(path2) != -1) {
 	obj.response.status = "SUBSCRIPTION_PURCHASED"
 	obj.response.expire_date = 4070880000000
 	obj.response.subscription_id = "com.picsart.studio.subscription_yearly"
 	obj.response.is_trial = false
+	obj.reason = "purchased"
 }
 $done({body: JSON.stringify(obj)})
