6cdad7ee78652ae95b970869e23002200ebed38b
Tingo <findjohntg@gmail.com>
2019-12-18 15:43:11 +0800

    update buy_itunes.js

--- buy_itunes.js
+++ buy_itunes.js
@@ -1,6 +1,7 @@
 let body= $response.body;
 var obj = JSON.parse(body);
 if (body.indexOf("expires") !=-1) {
+  obj["receipt"]["in_app"][0]["is_trial_period"] = false
   obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
   obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
   obj["receipt"]["in_app"][0]["expires_date_ms"] = "4096019658000";
--- douban_vip.js
+++
@@ -1,5 +0,0 @@
-let obj = JSON.parse($response.body);
-obj.vip_end_time = "2099-11-11T00:00:00+08:00";
-obj.is_vip = true;
-$done({body: JSON.stringify(obj)});
-//https:\/\/read\.douban\.com\/api\/v2\/people($|\?x_includes=is_vip%2Cvip_end_time%2Cis_auto_renewable)
