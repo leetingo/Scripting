9cfa715236f6bb7f721e3a8a6914504aabfc4237
Tingo <findjohntg@gmail.com>
2019-12-30 22:13:28 +0800

    Update Bahamut.js

--- QuantumultX/Bahamut.js
+++ QuantumultX/Bahamut.js
@@ -23,79 +23,80 @@ if ($request.headers) {
 
         $task.fetch(EndAd).then(response => {
             if (response.status == 200) {
-                $task.fetch(EndAd).then(respose => {
+                $task.fetch(EndAd).then(response => {
                     if (response.status == 200) {
-                        $task.fetch(EndAd).then(response => {}, reason => {
+                        $task.fetch(EndAd).then(reason => {
                             if (reason.error) {
                                 console.log(error)
                                 $done()
                             }
                         });
                     }
-				},	reason => {
-                        if (reason.error) {
-                            console.log(reason.error)
-                            $done()
-                        }
-                    });
-                }
-            }, reason => {
-                if (reason.error) {
-                    console.log(reason.error)
-                    $done()
-                }
-            });
-        }
-        $done({})
-    } else {
-        var body = $response.body;
-        var obj = JSON.parse(body);
-        if (url.indexOf('video') != -1) {
-            var snn = obj['video']['video_sn']
-                var StartAd = {
-                url: 'https://api.gamer.com.tw/mobile_app/anime/v1/stat_ad.php?ad=&schedule=0&sn=' + snn,
-                method: 'GET',
-                headers: {
-                    "Cookie": $prefs.valueForKey("CookieDHF"),
-                    "User-Agent": $prefs.valueForKey("UADHF"),
-                }
+                }, reason => {
+                    if (reason.error) {
+                        console.log(reason.error)
+                        $done()
+                    }
+                });
             }
-            $task.fetch(StartAd).then(response => {
+        }, reason => {
+            if (reason.error) {
+                console.log(reason.error)
+                $done()
+            }
+        });
+    }
+    $done({})
+} else {
+    var body = $response.body;
+    var obj = JSON.parse(body);
+    if (url.indexOf('video') != -1) {
+        var snn = obj['video']['video_sn']
+        var StartAd = {
+            url: 'https://api.gamer.com.tw/mobile_app/anime/v1/stat_ad.php?ad=&schedule=0&sn=' + snn,
+            method: 'GET',
+            headers: {
+                "Cookie": $prefs.valueForKey("CookieDHF"),
+                "User-Agent": $prefs.valueForKey("UADHF"),
+            }
+        }
+        $task.fetch(StartAd).then(response => {
             if (response.status == 200) {
-                $task.fetch(StartAd).then(respose => {
+                $task.fetch(StartAd).then(response => {
                     if (response.status == 200) {
-                        $task.fetch(StartAd).then(response => {
-							setTimeout(function(){$done({})}, 500)
-						}, reason => {
+                        $task.fetch(StartAd).then(reason => {
                             if (reason.error) {
                                 console.log(error)
                                 $done()
                             }
+                            else {
+                                setTimeout(function () { $done({}) }, 500)
+                            }
                         });
                     }
-				},	reason => {
-                        if (reason.error) {
-                            console.log(reason.error)
-                            $done()
-                        }
-                    });
-                }
-            }, reason => {
-                if (reason.error) {
-                    console.log(reason.error)
-                    $done()
-                }
-            });
-            //  $done({})
-        }
-        if (url.indexOf('token') != -1) {
-            obj['ad']['minor'] = [];
-            obj['ad']['major'] = [];
-            body = JSON.stringify(obj)
-                setTimeout(function () {
-                    $done({
-                        body
-                    })
-                }, 1000)
-        }
+                }, reason => {
+                    if (reason.error) {
+                        console.log(reason.error)
+                        $done()
+                    }
+                });
+            }
+        }, reason => {
+            if (reason.error) {
+                console.log(reason.error)
+                $done()
+            }
+        });
+        //  $done({})
     }
+    if (url.indexOf('token') != -1) {
+        obj['ad']['minor'] = [];
+        obj['ad']['major'] = [];
+        body = JSON.stringify(obj)
+        setTimeout(function () {
+            $done({
+                body
+            })
+        }, 1000)
+    }
+}
