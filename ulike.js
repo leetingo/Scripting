d68be9d008e6811e89c5d9c073bb76e62a0b1059
Tingo <TingoLee@users.noreply.github.com>
2020-01-29 17:35:16 +0800

    Update ulike.js

--- ulike.js
+++ ulike.js
@@ -1,16 +1,7 @@
 let obj = JSON.parse($response.body);
-obj ={
-"ret":"0",
-"errmsg":"Success",
-"systime":"",
-"data":{
-"flag":true,
-"start_time":1579094492,
-"end_time":3725012184,
-"is_first_subscribe":false,
-"is_cancel_subscribe":false,
-"uid":"4089778978429341",
-"subscribe_uid":"70851259049"
-}
-};
+obj.data["end_time"] = 3725012184
+obj.data["start_time"] = 1579094492
+obj.data["flag"] = true
+obj.data["is_first_subscribe"] = false
+obj.data["is_cancel_subscribe"] = false
 $done({body: JSON.stringify(obj)});
