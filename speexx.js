01ceddee9b41c0cca7eefe4f6112c8f0b75d0a00
Tingo <findjohntg@gmail.com>
2020-05-05 22:18:26 +0800

    Create speexx.js

---
+++ speexx.js
@@ -0,0 +1,20 @@
+let obj = JSON.parse($request.body);
+if($request.method == "PUT") {
+	obj.elapsed = randomNum(200, 1000);
+	obj.result = 100;
+}
+$done({body: JSON.stringify(obj)});
+	
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
