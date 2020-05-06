7c2f58a074d555fac7324ce71a989cf0affda512
Tingo <findjohntg@gmail.com>
2020-05-06 11:43:30 +0800

    Update speexx_final.js

--- speexx_final.js
+++ speexx_final.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($request.body);
 if($request.method == "PUT") {
-	obj.elapsed = randomNum(120, 180);
+	obj.elapsed = randomNum(60, 120);
 	obj.result = 100;
 }
 $done({body: JSON.stringify(obj)});
