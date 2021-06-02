00b455177e635a2ba4f6a77c69c580ff20efcf49
Tingo <TingoLee@users.noreply.github.com>
2021-06-02 22:21:12 +0800

    fix vsco.js

--- vsco.js
+++ vsco.js
@@ -2,6 +2,6 @@ let body = $response.body;
 let obj = JSON.parse(body);
 obj["subscriber"]["subscriptions"]["VSCOANNUAL"]["unsubscribe_detected_at"] = null;
 obj["subscriber"]["subscriptions"]["VSCOANNUAL"]["expires_date"] = "2099-11-11T00:00:00Z";
-obj["subscriber"]["entitlements"]["VSCOANNUAL"]["expires_date"] = "2099-11-11T00:00:00Z";
+obj["subscriber"]["entitlements"]["membership"]["expires_date"] = "2099-11-11T00:00:00Z";
 body = JSON.stringify(obj);
 $done({body})
