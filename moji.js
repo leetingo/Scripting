b69206beda01c5d4b692f9d6ac4337cc3a85538b
Tingo <TingoLee@users.noreply.github.com>
2021-06-26 14:22:35 +0800

    Update moji.js

--- moji.js
+++ moji.js
@@ -1,7 +1,7 @@
 let obj = JSON.parse($response.body)
 for(let i = 0; i < obj.result.result.length; i++)
 {
-    obj.result.result[i]["privilegeStatus"] = "Activated"
-    obj.result.result[i]["canPay"] = false
+    obj.result.result[i]["privilege"]["purchaseDate"] = "1589631265675"
+    obj.result.result[i]["privilegeStatus"] = "activated"
 }
 $done({body: JSON.stringify(obj)})
